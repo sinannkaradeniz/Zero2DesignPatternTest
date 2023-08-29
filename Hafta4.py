eb1713c95c07c2e1907fdf1addbff75f26446c42
sinannkaradeniz <141728198+sinannkaradeniz@users.noreply.github.com>
2023-08-29 13:57:53 +0000

    function_hafta4

---
+++ Hafta4.py
@@ -0,0 +1,21 @@
+#fruits = ["apple", "banana", "cherry"]
+
+#print(fruits[0])
+
+#fruits[1] = "grape"
+
+#fruits.append("orange")
+#fruits.remove("cherry")
+#print(fruits)
+#fruits.pop()
+
+#print(fruits)
+
+
+point = (3,5,"t",2.55,5,7)
+
+x,y,d,t,y,u=point
+
+print("x:",x)
+print("y:",y)
+
